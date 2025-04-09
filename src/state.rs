6345da001a52323033a4346fcfec5bbfdc0d1a5b
tongxi_hu <tongxi_hu@jiliguala.com>
2025-04-09 14:37:54 +0800

    fix visibility

--- src/state.rs
+++ src/state.rs
@@ -129,7 +129,7 @@ impl<'a> State<'a> {
         let bind_group_layout = device.create_bind_group_layout(&wgpu::BindGroupLayoutDescriptor {
             entries: &[wgpu::BindGroupLayoutEntry {
                 binding: 0,
-                visibility: wgpu::ShaderStages::VERTEX | wgpu::ShaderStages::FRAGMENT,
+                visibility: wgpu::ShaderStages::VERTEX_FRAGMENT,
                 ty: wgpu::BindingType::Buffer {
                     ty: wgpu::BufferBindingType::Uniform,
                     has_dynamic_offset: false,
